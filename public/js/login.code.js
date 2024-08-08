d932e792d69b1a859a5fe9280a1d5bf83b40aa17
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-08-08 15:12:49 -0400

    Add show password functionality

--- public/js/login.code.js
+++ public/js/login.code.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+  
   $("form").submit(function (event) {
     event.preventDefault();
     console.log("Will send login request!");
+  });
+
+  $('#show-password-check input').click(function() {
+    const input = $('#password-input')[0];
+    console.log("input: ", input);
+    console.log("input.type: ", input.type);
+    input.type = input.type === "password" ? "text" : "password";
   });
 });
