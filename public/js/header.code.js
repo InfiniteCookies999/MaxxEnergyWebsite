27ac238832c959035a1c44fcc0baac3c0c36aed4
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-08-12 15:50:50 -0400

    Fixed header loading code

--- public/js/header.code.js
+++ public/js/header.code.js
@@ -1,7 +1,26 @@
-$(function () {
-  const includes = $('[data-include]')
-  $.each(includes, function () {
-    const file = $(this).data('include') + '.html';
-    $(this).load(file);
-  })
-})
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelectorAll("[data-include]").forEach((include) => {
+    const file = include.getAttribute("data-include") + ".html";
+    
+    // See: https://www.youtube.com/watch?v=yUGXqaV-qVw
+    const request = new XMLHttpRequest();
+    request.open("GET", file, true);
+    request.onreadystatechange = () => {
+      // Make sure it is done loading.
+      if (request.readyState !== 4) {
+        return;
+      }
+      // Check that the status code of the file is good.
+      if (request.status !== 200) {
+        console.log(`Error loading file "${file}" with status code ${request.status}`);
+        return;
+      }
+
+      include.innerHTML = request.response;
+
+    };
+    request.send();
+
+  });
+});
