fb59ee9e7e25792f894042a70b23bf856cc59e05
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-08-14 15:55:35 -0400

    Abstract constants into UserRepository

--- database/user.repository.js
+++ database/user.repository.js
@@ -2,15 +2,19 @@ const getDBConnection = require('./connection');
 
 class UserRepository {
 
-  static maxNameLength() {
+  maxNameLength() {
     return 40;
   }
 
-  static maxAddressLineLength() {
+  maxAddressLineLength() {
     return 250;
   }
 
-  static getValidStates() {
+  maxPasswordLength() {
+    return 100;
+  }
+
+  validStates() {
     return [
       'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM',
       'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
@@ -24,18 +28,21 @@ class UserRepository {
   async create() {
     
     const conn = await getDBConnection();
-    const enumStates = UserRepository.getValidStates()
+    const enumStates = this.validStates()
       .map((s) => "'" + s + "'").join();
 
+    // NOTE: Password does not use the maxPasswordLength here because it
+    // is a hashed version that gets stored in the database and not the
+    // length of the raw passwords the user sends.
     conn.query(`CREATE TABLE IF NOT EXISTS user (
-      first_name CHAR(${UserRepository.maxNameLength()}),
-      last_name CHAR(${UserRepository.maxNameLength()}),
+      first_name CHAR(${this.maxNameLength()}),
+      last_name CHAR(${this.maxNameLength()}),
       email TEXT(320),
       phone CHAR(12),
       state CHAR(2),
       county CHAR(100),
-      address_line_1 CHAR(${UserRepository.maxAddressLineLength()}),
-      address_line_2 CHAR(${UserRepository.maxAddressLineLength()}),
+      address_line_1 CHAR(${this.maxAddressLineLength()}),
+      address_line_2 CHAR(${this.maxAddressLineLength()}),
       zip_code ENUM(${enumStates}),
       password CHAR(120),
       join_date DATE
