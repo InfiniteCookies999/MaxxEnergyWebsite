e34883e813e0994dd2a00ac29f1f2eb05832d262
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-08-21 14:18:53 -0400

    Start on updating user profile information

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,12 @@
 const errorHandler = require('./error.handler');
 const controller = require('./controller');
 const HttpError = require('./http.error');
-const { validateBody } = require('./validation');
+const { validateBody, validateLoggedIn } = require('./validation');
 
 module.exports = {
   errorHandler,
   controller,
   HttpError,
-  validateBody
+  validateBody,
+  validateLoggedIn
 };
