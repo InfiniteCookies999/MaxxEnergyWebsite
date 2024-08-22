375a7f5484ab8a9b752758b3ac86e79c5af9fe52
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-08-22 16:47:11 -0400

    trying to support import replacements

--- middleware/reroute.js
+++ middleware/reroute.js
@@ -9,6 +9,18 @@ function replaceRoutes(body) {
   });
 }
 
+async function replaceInBody(body) {
+  // Begin by replacing routes.
+  body = replaceRoutes(body);
+  // Replace import tags with their bodies.
+  return await body.replaceAll(/<import\s*href="(.*)"><\/import>/g, async (_, p1) => {
+    // Let us start by finding the html file.
+    let filePath = path.join(__dirname, '../public', p1 + ".html");
+    const body = await fs.promises.readFile(filePath + '.html', 'utf8');
+    return body;
+  });
+}
+
 async function reroute(req, res, next) {
   if (req.method === 'GET') {
     const pathExtension = path.extname(req.url).toLowerCase();
@@ -32,14 +44,14 @@ async function reroute(req, res, next) {
         
         const body = await fs.promises.readFile(filePath + '.html', 'utf8');
         res.setHeader('Content-Type', 'text/html');
-        res.send(replaceRoutes(body));
+        res.send(replaceInBody(body));
 
       } else if (fs.existsSync(filePath + '.hbs')) {
         // We have to replace at the point of send because the hbs
         // engine needs to do it's work first!
         const osend = res.send;
         res.send = function (body) {
-          return osend.call(this, replaceRoutes(body));
+          return osend.call(this, replaceInBody(body));
         };
         next();
       } else {
