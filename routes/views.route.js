ce5c6118b42ec6a55a4536f740bf19de53c10176
Christian Kim <Christian Kim>
2024-08-30 13:45:34 -0400

    commiting

--- routes/views.route.js
+++ routes/views.route.js
@@ -15,10 +15,7 @@ function getReroute() {
 // https://stackoverflow.com/questions/14127411/use-a-route-as-an-alias-for-another-route-in-express-js
 router.get(['/', '/index', '/home', '/main'], controller(async (req, res) => {
   res.render('index', {
-    preloadImage1: '/webdev/images/homepage.jpg',
-    preloadImage2: '/webdev/images/homepage2.jpg',
-    preloadImage3: '/webdev/images/homepage3.jpg',
-    isLoggedIn: !!req.session.user // Pass this variable to indicate login status
+    preloadImage1: '/images/homepage1.jpg',
   });
 }));
 
